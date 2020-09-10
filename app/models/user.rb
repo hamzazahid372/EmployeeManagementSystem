dae53d5a91c05afbc6f1ed57db83015c45984826
kamran7vals <kamran.khalid@7vals.com>
2020-09-10 18:26:11 +0500

    Before migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@
 
 # Users model
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   belongs_to :company
   belongs_to :department
   belongs_to :team
