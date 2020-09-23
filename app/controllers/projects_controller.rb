45cf116f0ccaddd9a68c0d51ac28502f52e97ca0
ArifaAbid <arifa.abid@7vals.com>
2020-09-23 13:59:43 +0500

    issues resolved

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,41 +26,48 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    respond_to do |format|
-      format.html
-    end
+    success = true
     if @project.save
       flash[:notice] = t 'project.created'
-      redirect_to @project
     else
       errors = ''
       @project.errors.full_messages.each do |msg|
         errors = errors + msg + ', '
       end
       flash[:error] = errors
-      render 'new'
+      success = false
+    end
+    respond_to do |format|
+      if success
+        format.html { redirect_to @project }
+      else
+        format.html { render 'new' }
+      end
     end
   end
 
   def update
-    respond_to do |format|
-      format.html
-    end
+    success = true
     if @project.update(project_params)
       flash[:notice] = t 'project.updated'
-      redirect_to @project
     else
-      render 'new'
+      success = false
+    end
+    respond_to do |format|
+      if success
+        format.html { redirect_to @project }
+      else
+        format.html { render 'new' }
+      end
     end
   end
 
   def destroy
-    respond_to do |format|
-      format.html
-    end
     if @project.destroy
       flash[:notice] = t 'project.destroyed'
-      redirect_to @project
+    end
+    respond_to do |format|
+      format.html { redirect_to @project }
     end
   end
 
@@ -70,3 +77,4 @@ class ProjectsController < ApplicationController
     params.require(:project).permit(:name, :description, :status, :start_date, :end_date, :expected_start_date, :expected_end_date, :sequence_num)
   end
 end
+
