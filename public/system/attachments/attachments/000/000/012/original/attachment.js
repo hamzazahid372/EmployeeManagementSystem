b7bd43a188e8bc0c60c9770ac4ab2ff5f2ecc076
ArifaAbid <arifa.abid@7vals.com>
2020-10-01 12:15:31 +0500

    removed

--- public/system/attachments/attachments/000/000/012/original/attachment.js
+++
@@ -1,5 +0,0 @@
-$(document).ready( function () {
-  $('#documents-tab').on('click', function(e){
-    $.ajax({ type:'GET', url: new URL(location.href).pathname + '/attachments.js' });
-  });
-});
--- public/system/attachments/attachments/000/000/015/original/user.js
+++
@@ -1,6 +0,0 @@
-require("datatables.net-bs4");
-document.currentScript.setAttribute("data-turbolinks-eval", "false");
-
-$(document).ready( function () {
-  $('#users-datatable').DataTable();
-});
