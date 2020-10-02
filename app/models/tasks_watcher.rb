c96303156910d20987049a8d06a49a6936037d6c
zhamza372 <hamza.zahid@7vals.com>
2020-10-02 15:06:03 +0500

    watcher added

--- app/models/tasks_watcher.rb
+++ app/models/tasks_watcher.rb
@@ -5,5 +5,5 @@ class TasksWatcher < ApplicationRecord
   belongs_to :company
   belongs_to :task
   belongs_to :watcher, polymorphic: true
-  validates :task_id, uniqueness: { scope: [:watcher_id :watcher_type] }
+  validates :task_id, uniqueness: { scope: %i[watcher_id watcher_type] }
 end
