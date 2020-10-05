f70e90d47797866747ba7055e0fde777364a49a2
kamran7vals <kamran.khalid@7vals.com>
2020-10-05 13:29:56 +0500

    Gemfile, schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_102510) do
+ActiveRecord::Schema.define(version: 2020_10_04_142731) do
 
   create_table "attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "company_id", null: false
